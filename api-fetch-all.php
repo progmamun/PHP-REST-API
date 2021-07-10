aaca9ec4e6a194b2754a90d4839383ef113b245e
progmamun <mamun333089@gmail.com>
2021-07-10 22:32:14 +0600

    Read date

--- api-fetch-all.php
+++ api-fetch-all.php
@@ -1,4 +1,15 @@
 <?php
+header('Content-type: application/json');
+header('Acess-Control-Allow-Origin: *');
 
+include "config.php";
 
-?>
+$sql = "SELECT * FROM students";
+$result = mysqli_query($conn, $sql) or die("SQL Query Failed.");
+
+if (mysqli_num_rows($result) > 0) {
+    $output = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    echo json_encode($output);
+} else {
+    echo json_encode(array('message' => 'No Record Found.', 'status' => false));
+}
