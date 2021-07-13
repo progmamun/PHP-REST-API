346ed342c2d0ff1df44fe654481c43cc80f2d0a1
progmamun <mamun333089@gmail.com>
2021-07-13 13:01:28 +0600

    create data

--- api-insert.php
+++ api-insert.php
@@ -1,5 +1,21 @@
 <?php
+header('Content-type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,content-type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
+$data = json_decode(file_get_contents("php://input"), true);
 
+$name = $data['sname'];
+$age = $data['sage'];
+$city = $data['scity'];
 
-?>
+include "config.php";
+
+$sql = "INSERT INTO test(student_name, age, city) VALUES ('{$name}',{$age},'{$city}')";
+
+if (mysqli_query($conn, $sql)) {
+    echo json_encode(array('message' => 'Student Record Inserted.', 'status' => true));
+} else {
+    echo json_encode(array('message' => 'Student Record Not Inserted.', 'status' => false));
+}
