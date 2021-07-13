ef92a02e3127e4cca56013ff7aaf3851499696ea
progmamun <mamun333089@gmail.com>
2021-07-13 13:12:30 +0600

    Update data

--- api-update.php
+++ api-update.php
@@ -1,5 +1,22 @@
 <?php
+header('Content-type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: PUT');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,content-type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
+$data = json_decode(file_get_contents("php://input"), true);
 
+$id = $data['sid'];
+$name = $data['sname'];
+$age = $data['sage'];
+$city = $data['scity'];
 
-?>
+include "config.php";
+
+$sql = "UPDATE test SET student_name ='{$name}', age = {$age}, city = '{$city}' WHERE id = {$id}";
+
+if (mysqli_query($conn, $sql)) {
+    echo json_encode(array('message' => 'Student Record Updated.', 'status' => true));
+} else {
+    echo json_encode(array('message' => 'Student Record Not Updated.', 'status' => false));
+}
